5cb979eab78b3362f3db9ffdbeece39f950c981f
Diana Maribel Calpa Galindres <61337482+101014638@users.noreply.github.com>
2025-04-29 14:29:02 -0500

    cambios

--- Frontend/src/app/services/universidades.service.ts
+++ Frontend/src/app/services/universidades.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UniversidadesService {
+  constructor(protected http: HttpClient) {}
 
-  constructor() { }
+  obteneruniversidad(): Observable<any> {
+    let ruta = [environment.apiUrl, 'universidad'].join('/');
+    return this.http.get(ruta);
+  }
+
+  obteneruniversidadId(id: any): Observable<any> {
+    let ruta = [environment.apiUrl,'universidad', id].join('/');
+    return this.http.get(ruta);
+  }
 }
